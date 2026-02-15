0d1cffbbb46ca8ad00670fa0b69a6f8c3f286eee
Louis Escher <louisescher@proton.me>
2026-02-15 20:05:41 +0100

    feat: Record creation prototype

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		server: {
+			host: "127.0.0.1",
+			port: 4321
+		}
+	}
+});
