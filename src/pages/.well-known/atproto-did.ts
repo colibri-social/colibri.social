b905298781b798237fec45028a278b498b2b4e06
Louis Escher <louisescher@proton.me>
2026-02-28 23:38:42 +0100

    Update atproto-did.ts

--- src/pages/.well-known/atproto-did.ts
+++ src/pages/.well-known/atproto-did.ts
@@ -2,13 +2,7 @@ import { SAME_TLD_DID } from "astro:env/server";
 
 export const GET = () => {
 	if (SAME_TLD_DID) {
-		return new Response(SAME_TLD_DID, {
-			status: 200,
-			statusText: "OK",
-			headers: new Headers({
-				"content-type": "text/plain",
-			}),
-		});
+		return new Response(SAME_TLD_DID);
 	} else {
 		return new Response(null, {
 			status: 404,
