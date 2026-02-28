f689edb632e0e63c658edcc3d5b69950bcb3ce81
Louis Escher <louisescher@proton.me>
2026-02-28 23:34:52 +0100

    feat: .well-known/atproto-did resolving if a SAME_TLD_DID is set

---
+++ src/pages/.well-known/atproto-did.ts
@@ -0,0 +1,17 @@
+import { SAME_TLD_DID } from "astro:env/server";
+
+export const GET = () => {
+	if (SAME_TLD_DID) {
+		return new Response(SAME_TLD_DID, {
+			status: 200,
+			statusText: "OK",
+			headers: new Headers({
+				"content-type": "plain/text",
+			}),
+		});
+	} else {
+		return new Response(null, {
+			status: 404,
+		});
+	}
+};
