e324d7ad488ef218f0cc53feadc90e50dbf17925
Louis Escher <louisescher@proton.me>
2026-03-02 16:49:21 +0100

    chore: Refactor settings modal into reusable component

--- src/components/solid/components/Community/CommunitySettingsModal.tsx
+++ src/components/solid/components/Community/CommunitySettingsModal.tsx
@@ -1,23 +1,4 @@
-import {
-	createEffect,
-	createSignal,
-	For,
-	Match,
-	Show,
-	Switch,
-	type Accessor,
-	type Component,
-	type ParentComponent,
-	type Setter,
-} from "solid-js";
-import {
-	Dialog,
-	DialogCloseButton,
-	DialogContent,
-	DialogPortal,
-	DialogTrigger,
-} from "../../shadcn-solid/Dialog";
-import { X } from "../../icons/X";
+import { createSignal, Match, Switch, type Component } from "solid-js";
 import {
 	TextField,
 	TextFieldInput,
@@ -41,52 +22,8 @@ import { XCircle } from "../../icons/XCircle";
 import { Button } from "../../shadcn-solid/Button";
 import { Spinner } from "../../icons/Spinner";
 import { actions } from "astro:actions";
-
-const SettingsPage: ParentComponent<{
-	loading: Accessor<boolean>;
-	title: string;
-	onSave?: () => void;
-	canReset?: boolean;
-	onReset?: () => void;
-}> = (props) => {
-	return (
-		<div class="w-full flex flex-col justify-between gap-4 h-128">
-			<div class=" flex flex-col gap-4 py-4">
-				<h2 class="m-0 px-4">{props.title}</h2>
-				<div class="w-full h-full flex flex-col gap-4 px-4">
-					{props.children}
-				</div>
-			</div>
-			<Show when={props.onSave || props.onReset}>
-				<div class="w-full border-t border-border p-4 flex flex-row items-center justify-end gap-2">
-					<Show when={props.canReset && props.onReset}>
-						<Button
-							variant="secondary"
-							onClick={props.onReset}
-							disabled={props.loading()}
-						>
-							Reset
-						</Button>
-					</Show>
-					<Show when={props.onSave}>
-						<Button
-							onClick={props.onSave}
-							disabled={props.loading() || !props.canReset}
-						>
-							<Spinner
-								classList={{
-									hidden: !props.loading(),
-									block: props.loading(),
-								}}
-							/>
-							Save
-						</Button>
-					</Show>
-				</div>
-			</Show>
-		</div>
-	);
-};
+import { SettingsModal, SettingsPage } from "../SettingsModal";
+import type { ParentComponent } from "solid-js";
 
 const GeneralSettingsPage: Component = () => {
 	const [globalData, { addCommunity }] = useGlobalContext();
@@ -330,88 +267,23 @@ const DangerSettingsPage: Component = () => {
 	);
 };
 
-type SettingsPage = "general" | "danger";
-
-const SettingsPageSelector: ParentComponent<{
-	onClick: Setter<SettingsPage>;
-	danger?: boolean;
-	activePage: boolean;
-}> = (props) => {
+export const CommunitySettingsModal: ParentComponent = (props) => {
 	return (
-		<button
-			type="button"
-			class="w-full hover:bg-muted/25 px-2 py-1 rounded-sm cursor-pointer text-left"
-			classList={{
-				"text-destructive hover:bg-destructive/15!": props.danger,
-				"bg-muted/25": props.activePage && !props.danger,
-				"bg-destructive/10!": props.activePage && props.danger,
+		<SettingsModal
+			pages={[
+				{
+					title: "Community Profile",
+					id: "general",
+					component: GeneralSettingsPage,
+				},
+			]}
+			dangerPage={{
+				title: "Danger Zone",
+				id: "danger",
+				component: DangerSettingsPage,
 			}}
-			onClick={props.onClick}
 		>
 			{props.children}
-		</button>
-	);
-};
-
-export const CommunitySettingsModal: ParentComponent = (props) => {
-	const [activePage, setActivePage] = createSignal<SettingsPage>("general");
-	const [open, setOpen] = createSignal(false);
-
-	createEffect(() => {
-		if (open() === false) return;
-
-		setActivePage("general");
-	});
-
-	const PAGES: Array<SettingsPage> = ["general"];
-	const LABELS: Record<SettingsPage, string> = {
-		general: "Community Profile",
-		danger: "Danger Zone",
-	};
-
-	return (
-		<Dialog open={open()} onOpenChange={setOpen}>
-			<DialogTrigger>{props.children}</DialogTrigger>
-			<DialogPortal>
-				<DialogContent class="w-[75vw] min-w-92 h-fit min-h-128 max-w-192! p-0 flex flex-row gap-0">
-					<div class="absolute top-4 right-4 flex items-center justify-center w-6 h-6 hover:bg-muted/50 cursor-pointer rounded-sm">
-						<DialogCloseButton class="absolute cursor-pointer">
-							<X />
-						</DialogCloseButton>
-					</div>
-					<div class="h-full min-h-128 flex flex-col justify-between p-4 w-64 border-r border-border">
-						<div class="h-full flex flex-col gap-1">
-							<For each={PAGES}>
-								{(item) => (
-									<SettingsPageSelector
-										activePage={activePage() === item}
-										onClick={() => setActivePage(item)}
-									>
-										{LABELS[item]}
-									</SettingsPageSelector>
-								)}
-							</For>
-						</div>
-						<SettingsPageSelector
-							activePage={activePage() === "danger"}
-							danger
-							onClick={() => setActivePage("danger")}
-						>
-							{LABELS["danger"]}
-						</SettingsPageSelector>
-					</div>
-					<Switch
-						fallback={<div>No settings page for this category found.</div>}
-					>
-						<Match when={activePage() === "general"}>
-							<GeneralSettingsPage />
-						</Match>
-						<Match when={activePage() === "danger"}>
-							<DangerSettingsPage />
-						</Match>
-					</Switch>
-				</DialogContent>
-			</DialogPortal>
-		</Dialog>
+		</SettingsModal>
 	);
 };
