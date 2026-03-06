8766363292b0ce39d0964ef70efc573bb885e4ee
Louis Escher <louisescher@proton.me>
2026-03-06 15:01:02 +0100

    Update LinkEmbed.tsx

--- src/components/solid/components/Message/LinkEmbed.tsx
+++ src/components/solid/components/Message/LinkEmbed.tsx
@@ -21,35 +21,44 @@ export const LinkEmbed: Component<{ uri: string }> = (props) => {
 	const [embedData] = createResource(props.uri, fetchEmbedData);
 
 	return (
-		<div
-			class="flex flex-col border-l-2 px-4 p-2 bg-card my-2 rounded-r-sm max-w-[416px]"
-			style={{ "border-color": embedData()?.themeColor }}
+		<Show
+			when={
+				embedData() &&
+				Object.keys(embedData()!).every(
+					(key) => !!embedData()![key as keyof ReturnType<typeof embedData>],
+				)
+			}
 		>
-			<span class="text-sm">{embedData()?.siteName}</span>
-			<a
-				class="font-medium w-fit text-(--primary-hover)! decoration-(--primary-hover) hover:underline"
-				href={props.uri}
-				target="_blank"
+			<div
+				class="flex flex-col border-l-2 px-4 p-2 bg-card my-2 rounded-r-sm max-w-[416px]"
+				style={{ "border-color": embedData()?.themeColor }}
 			>
-				{embedData()?.title}
-			</a>
-			<span class="font-light text-sm">{embedData()?.description}</span>
-			<Show when={embedData()?.image}>
-				<img
-					width={400}
-					height={210}
-					class="w-full h-auto rounded-xs my-2 bg-muted border-none"
-					src={
-						embedData()?.image![0].url.includes("http")
-							? embedData()?.image![0].url
-							: new URL(props.uri).protocol +
-								"//" +
-								new URL(props.uri).host +
-								embedData()?.image![0].url
-					}
-					alt={embedData()?.image![0].alt || ""}
-				/>
-			</Show>
-		</div>
+				<span class="text-sm">{embedData()?.siteName}</span>
+				<a
+					class="font-medium w-fit text-(--primary-hover)! decoration-(--primary-hover) hover:underline"
+					href={props.uri}
+					target="_blank"
+				>
+					{embedData()?.title}
+				</a>
+				<span class="font-light text-sm">{embedData()?.description}</span>
+				<Show when={embedData()?.image}>
+					<img
+						width={400}
+						height={210}
+						class="w-full h-auto rounded-xs my-2 bg-muted border-none"
+						src={
+							embedData()?.image![0].url.includes("http")
+								? embedData()?.image![0].url
+								: new URL(props.uri).protocol +
+									"//" +
+									new URL(props.uri).host +
+									embedData()?.image![0].url
+						}
+						alt={embedData()?.image![0].alt || ""}
+					/>
+				</Show>
+			</div>
+		</Show>
 	);
 };
