604c03d03d76d85fe72c2f8eea919dda4fd4ec96
Louis Escher <louisescher@proton.me>
2026-04-24 16:25:46 +0200

    chore: dark mode in dev

--- packages/client/src/components/AppLoadingScreen.tsx
+++ packages/client/src/components/AppLoadingScreen.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 
 export const AppLoadingScreen: Component = () => {
 	return (
-		<div class="w-screen h-screen absolute top-0 left-0 z-50 flex items-center justify-center bg-background text-text">
+		<div class="w-screen h-screen absolute top-0 left-0 z-50 flex items-center justify-center">
 			Loading...
 		</div>
 	);
