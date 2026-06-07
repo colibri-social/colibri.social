1a391f9e2c54d102674123df71fec5d89ba5d5d0
Reuben Tier <otterlord.dev@gmail.com>
2026-06-07 12:17:46 +0100

    Fix build

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup";
 import { solidPlugin } from 'esbuild-plugin-solid';
+import Icons from 'unplugin-icons/esbuild';
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
   dts: true,
   clean: true,
-  esbuildPlugins: [solidPlugin()],
+  esbuildPlugins: [Icons({ compiler: 'solid' }), solidPlugin()],
 })
