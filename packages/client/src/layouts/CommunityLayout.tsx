4c48140873e570b1eb196b0da33428f8fbe0f430
Louis Escher <louisescher@proton.me>
2026-07-11 22:32:42 +0200

    Update CommunityLayout.tsx

--- packages/client/src/layouts/CommunityLayout.tsx
+++ packages/client/src/layouts/CommunityLayout.tsx
@@ -219,8 +219,8 @@ const CommunityLayout: ParentComponent = (props) => {
 				ref={(el) => createSwipe(el, swipe)}
 				class="flex flex-col"
 				classList={{
-					"w-full h-full max-h-[calc(100vh-41px)]":
-						!isMobile() && !isTauriRuntime(),
+					"w-full h-full": !isMobile(),
+					"max-h-[calc(100vh-41px)]": !isMobile() && !isTauriRuntime(),
 					"max-w-[calc(100vw-576px-56px-1px)]":
 						!isMobile() &&
 						!displayMembersAsSheet() &&
