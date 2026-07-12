d18620eb6f248d14ecda7aa508fff59fe7b0bd88
Louis Escher <louisescher@proton.me>
2026-07-12 21:25:57 +0200

    Update build.gradle.kts

--- packages/wrapper/src-tauri/gen/android/app/build.gradle.kts
+++ packages/wrapper/src-tauri/gen/android/app/build.gradle.kts
@@ -28,15 +28,15 @@ android {
     signingConfigs {
         create("release") {
             val keystorePropertiesFile = rootProject.file("keystore.properties")
-            val keystoreProperties = Properties()
             if (keystorePropertiesFile.exists()) {
+                val keystoreProperties = Properties()
                 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["password"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["password"] as String
             }
-    
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["password"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["password"] as String
         }
     }
     buildTypes {
